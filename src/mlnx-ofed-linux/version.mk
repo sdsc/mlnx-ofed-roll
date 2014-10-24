6a4aa4d2aaa622b109e741feef6a8dfd1ffa8944
Trevor Cooper <tcooper@sdsc.edu>
2014-10-24 08:14:27 -0700

    Build mlnx-ofed-linux from source ISO

--- src/mlnx-ofed-linux/version.mk
+++ src/mlnx-ofed-linux/version.mk
@@ -1,7 +1,18 @@
-PKGROOT         = /tmp/mlnx-ofed-linux
-NAME            = mlnx-ofed-linux
-VERSION         :=$(shell bash ../../version.sh -v)
-RELEASE         :=$(shell bash ../../version.sh -h)
-COPYRIGHT       = Copyright (c) 2014, The Regents of the University of California.
+NAME              = mlnx-ofed-linux
+VERSION           = 2.3
+RELEASE           = 1.0.1
+EXTRA             = rhel6.5-x86_64
+PKGROOT           = /opt/mlnx-ofed-linux
 
-FILES           = ofed-vma.conf
+SRC_SUBDIR        = mlnx-ofed-linux
+
+SOURCE_NAME       = MLNX_OFED_LINUX
+SOURCE_SUFFIX     = tgz
+SOURCE_VERSION    = $(VERSION)-$(RELEASE)-$(EXTRA)-ext
+SOURCE_PKG        = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR        = $(SOURCE_NAME)-$(SOURCE_VERSION)
+
+ISO_NAME          = $(SOURCE_NAME)-$(VERSION)-$(RELEASE)-$(EXTRA).iso
+ISO_PKGS          = $(ISO_NAME)
+
+RPM.EXTRAS        = AutoReq:No
