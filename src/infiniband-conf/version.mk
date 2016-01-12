1233e22f44ab131a223b5be9bd09d3fb01685a4a
Trevor Cooper <tcooper@sdsc.edu>
2016-01-12 14:51:15 -0800

    APS-242: Remove rpm conflicting with kmod-mlnx-ofa_kernel

--- src/infiniband-conf/version.mk
+++
@@ -1,7 +0,0 @@
-PKGROOT         = /etc/infiniband
-NAME            = mlnx-ofed-infiniband-conf
-VERSION         :=$(shell bash ../../version.sh -v)
-RELEASE         :=$(shell bash ../../version.sh -h)
-COPYRIGHT       = Copyright (c) 2014, The Regents of the University of California.
-
-FILES           = connectx.conf openibd.conf
