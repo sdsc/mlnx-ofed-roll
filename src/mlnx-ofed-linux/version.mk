e035c6958a73dac358abb77418f98fda0618b4f8
Christopher Irving <cirving@sdsc.edu>
2016-10-19 15:44:46 -0700

    Changes need to build version 3.3 of Mellanox ofed

--- src/mlnx-ofed-linux/version.mk
+++ src/mlnx-ofed-linux/version.mk
@@ -1,7 +1,7 @@
 NAME              = mlnx-ofed-linux
-VERSION           = 3.1
-RELEASE           = 1.0.3
-EXTRA             = rhel6.7-x86_64
+VERSION           = 3.3
+RELEASE           = 1.0.4.0
+EXTRA             = rhel6.8-x86_64
 PKGROOT           = /opt/mlnx-ofed-linux
 
 SRC_SUBDIR        = mlnx-ofed-linux
