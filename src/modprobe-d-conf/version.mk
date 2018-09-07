caca57dd96021be5852c13ad1bdd6720208d6218
Trevor Cooper <tcooper@sdsc.edu>
2018-09-07 15:05:45 -0700

    Remove configuration RPM from roll
    The mlnx-ofed-modprobe-d-conf RPM can/will overwrite custom configuration deployed by the administrator if the mlnx-ofed roll is updated on the system. Since Rocks doesn't provide an easy way to define RPM configuration file replacement behavior it's easiest to completely remove this RPM from the repository.

--- src/modprobe-d-conf/version.mk
+++
@@ -1,7 +0,0 @@
-PKGROOT         = /etc/modprobe.d
-NAME            = mlnx-ofed-modprobe-d-conf
-VERSION         :=$(shell bash ../../version.sh -v)
-RELEASE         :=$(shell bash ../../version.sh -h)
-COPYRIGHT       = Copyright (c) 2014, The Regents of the University of California.
-
-FILES           = mlx4_core.conf
