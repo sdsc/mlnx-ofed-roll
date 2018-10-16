c7d4a5f73ba5e4e96bb9c6f82fd08f357899b78e
Trevor Cooper <tcooper@sdsc.edu>
2018-10-16 10:32:10 -0700

    Whitespace fixups

--- version.mk
+++ version.mk
@@ -1,10 +1,9 @@
-VERSION         :=$(shell bash version.sh -v)
-RELEASE         :=$(shell bash version.sh -h)
-COPYRIGHT       = Copyright (c) 2014, The Regents of the University of California.
-COLOR		= lavenderblush
 MLNX           :=$(shell bash mlnx.sh)
 KERN           :=$(shell bash kern.sh)
 ROLLNAME        = mlnx-ofed-$(MLNX)-$(KERN)
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
 COPYRIGHT       = Copyright (c) 2018, The Regents of the University of California.
+COLOR           = lavenderblush
 
-REDHAT.ROOT	= $(CURDIR)
+REDHAT.ROOT     = $(CURDIR)
