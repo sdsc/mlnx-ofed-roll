38478948c0aa4743ace3feacb7576e867109981b
Trevor Cooper <tcooper@sdsc.edu>
2018-10-16 10:31:55 -0700

    Copyright update

--- version.mk
+++ version.mk
@@ -5,5 +5,6 @@ COLOR		= lavenderblush
 MLNX           :=$(shell bash mlnx.sh)
 KERN           :=$(shell bash kern.sh)
 ROLLNAME        = mlnx-ofed-$(MLNX)-$(KERN)
+COPYRIGHT       = Copyright (c) 2018, The Regents of the University of California.
 
 REDHAT.ROOT	= $(CURDIR)
